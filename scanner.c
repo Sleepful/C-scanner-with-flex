4b4232b9aa4dca41d8453dafb9706429e27e300f
Sleepful <sleepfuldesire@gmail.com>
2017-09-18 05:19:23 -0600

    FUNCIONA PAPUS

--- scanner.c
+++ scanner.c
@@ -2,26 +2,33 @@
 #include <string.h>
 #include <stdlib.h>
 #include "scanner.h"
+#define TKNS 11
+
 
 extern int yylex();
 extern int yylineno;
 extern char* yytext;
-int yylinecounter = 10;
+int yylinecounter = 250;
 int yylinelastcount= 1;
 
 typedef struct table_row 
 	{
 	int token;
-	char* name;
+	char* text;
 	struct table_row * next;
 	} 
 	Row;
 
-Row* table[11]; //symbol table 4 later
-int tokencount[11];
-// *tokencount  [1];  //this should be a dynamic array of tokencount arrays
-
 Row getToken(void);
+void writetoken(Row rowtoken, FILE* file);
+void beginwritting(FILE* file);
+void finishwritting(FILE* file);
+
+Row* table[TKNS]; //symbol table 4 later
+int tokencount[TKNS];
+// *tokencount  [1];  //this should be a dynamic array of tokencount arrays
+char* names[TKNS] = {"KEYWORD","IDENTIFIER","CONSTANTLITERAL","OPERATOR","PUNCTUATOR","COMMENT","PREPROCESSOR","CONSTANTCHAR","CONSTANTSTRING","ERROR","BLANK"};
+char* colors[TKNS] = {"BurntOrange","Aquamarine","ForestGreen","Goldenrod","Fuchsia","Rhodamine","Gray","GreenYellow","Emerald","Red","White"};
 
 FILE* file;
 char* texsource = "source.tex";
@@ -31,32 +38,59 @@ int main(void)
 	file = fopen(texsource,"w");
 	Row rowtoken;
 	rowtoken = getToken();
+	beginwritting(file);
 	while(rowtoken.token) 
 		{
-		fprintf(file,"%d\n", rowtoken.token);
-		fprintf(file,"%s\n",rowtoken.name);
-		if(yylineno>=(yylinelastcount+yylinecounter))
-			{
-			yylinelastcount=yylinelastcount+yylinecounter;
-			fprintf(file,"new line counter reached: %d\n",yylineno);
-			}
+		writetoken(rowtoken,file);
 		rowtoken = getToken();
 		}
+	finishwritting(file);
 	return 0;
 	}
+
+void beginwritting(FILE* file)
+	{
+	fprintf(file,"\\begin{frame}[fragile,allowframebreaks]{Syntax Highlighting}~");
+	}
+void finishwritting(FILE* file)
+	{
+	fprintf(file,"\n\\end{frame}\n");
+	}
+
+void writetoken(Row rowtoken,FILE* file)
+		{
+		if(yylineno>=(yylinelastcount+yylinecounter))
+			{
+			fprintf(file,"\n\\end{frame}\n\\begin{frame}[fragile,allowframebreaks]{Syntax Highlighting}~");
+			yylinelastcount=yylinelastcount+yylinecounter;
+			//fprintf(file,"new line counter reached: %d\n",yylineno);
+			}
+		if(rowtoken.token==BLANK)
+			{
+			if(rowtoken.text[0]==0x9)
+				fprintf(file,"\\tab");
+			if(rowtoken.text[0]==0xa)
+				fprintf(file,"\\newline");
+			if(rowtoken.text[0]==' ')
+				fprintf(file," ");
+			}
+		if(rowtoken.token==COMMENT)
+			{
+			fprintf(file,"\\color{%s}\\begin{verbatim}%s\\end{verbatim}\\leavevmode",colors[rowtoken.token-1],rowtoken.text);
+			}
+		if(rowtoken.token!=COMMENT&&rowtoken.token!=BLANK)
+			{
+			fprintf(file,"\\color{%s}\\verb$%s$", colors[rowtoken.token-1],rowtoken.text);
+			}
+		}
+
 
 Row getToken(void)
 	{
 	Row myRow;
 	int token = yylex();
 	myRow.token = token;
-	myRow.name = yytext;
+	myRow.text = yytext;
 	return myRow;
 	}
 
-void writeToEOF(char* text, int token)
-	{
-	//f = fopen("contacts.pcl", "a");
-	//this is incompletely incomplete
-	//fclose(f);
-	}
